61ff38d97bde3d37ebd91450d70034582e92e464
Ayush Shukla <ashukla@groupon.com>
2018-12-12 22:56:30 +0530

    adding cors support

--- src/main/java/com/filters/ResponseServerFilter.java
+++ src/main/java/com/filters/ResponseServerFilter.java
@@ -13,5 +13,7 @@ public class ResponseServerFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     }
 }
