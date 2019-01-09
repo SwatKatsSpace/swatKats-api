803762a4182d9d59aecba9730b3a5fc29b110161
Ayush Shukla <ashukla@groupon.com>
2019-01-09 22:20:57 +0530

    updating .sql

--- src/main/resources/insert_schema.sql
+++ src/main/resources/insert_schema.sql
@@ -6,3 +6,5 @@ VALUES ('Ram', 'Babu', 'ram.babu@gmail.com', '8888877777', '1111222233334444', '
 
 INSERT INTO article (firstName, lastName, email, phone, aadhar_id, pan_id, image, relation, description, likes)
 VALUES ('Lata', 'Kumari', 'lata.kumari@gmail.com', '8888877777', '1111222233334444', 'DDDDD44444', 'https://www.ethicsdaily.com/wp-content/uploads/2018/07/church-state-both-should-seek-image-24450.jpg', 'Servant', 'Christians have sometimes claimed that the eighth commandment, You shall not steal, forbids government from ever mandating the redistribution of wealth for the sake of the poor.', '22');
+
+ UPDATE article SET image = 'https://www.ethicsdaily.com/wp-content/uploads/2018/07/church-state-both-should-seek-image-24450.jpg' WHERE firstName = 'Ram';
