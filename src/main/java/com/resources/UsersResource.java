a623c4275eb685946bae74cf7dc660c1f38ea916
Ayush Shukla <ashukla@groupon.com>
2019-02-03 14:42:12 +0530

    removing duplicate code

--- src/main/java/com/resources/UsersResource.java
+++ src/main/java/com/resources/UsersResource.java
@@ -56,45 +56,4 @@ public class UsersResource {
         userService.createUser(user);
         return Response.ok(201).build();
     }
-    //    @GET
-    //    @Path("/")
-    //    public User getUserByUuid(@QueryParam("email") String email) {
-    //        return userService.getUserByEmail(email);
-    //    }
-
-    //    @POST
-    //    @Consumes(MediaType.APPLICATION_JSON)
-    //    public GenericResponse insertUser(ImmutableUser user) {
-    //        LOGGER.info("getting user info");
-    //        List<ImmutableUser> immutableUsers = userDAO.findByEmailAndPhoneAndAadhar(user.email(), user.phone().get(), user.aadharId());
-    //        if (immutableUsers == null || immutableUsers.isEmpty()) {
-    //            Integer result = userDAO.insert(user.name(), user.email(), user.phone().toString(), user.password(), user.aadharId(), user.panId());
-    //            if (result > 0) {
-    //                return new GenericResponse(1, "Sucessfully Updated");
-    //            } else {
-    //                return new GenericResponse(0, "Failed to Updated");
-    //            }
-    //        } else {
-    //            return new GenericResponse(0, "Duplicate Records");
-    //        }
-    //
-    //
-    //    }
-    //
-    //    @GET
-    //    @Path("/completeInfo")
-    //    public List<UserCompleInfo> getAllCompleteUserDetails() {
-    //        List<ImmutableUser> users = userDAO.getAll();
-    //        List<UserCompleInfo> userCompleInfos = new ArrayList<UserCompleInfo>();
-    //        users.stream().map(u -> {
-    //            UserCompleInfo userCompleInfo = ImmutableUserCompleInfo.builder()
-    //                    .user(u)
-    //                    .userDetails(userDetailsDAO.findId(u.uuid()))
-    //                    .build();
-    //            return userCompleInfos.add(userCompleInfo);
-    //        });
-    //        return userCompleInfos;
-    //    }
-
-
 }
