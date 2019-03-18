44ad6e8c3c544eb28119d9a49067f39d213b3810
Jay <jay@Jays-MacBook-Pro.local>
2019-03-18 13:30:29 +0530

    adding provider annotation

--- src/main/java/com/service/interceptor/RequestInterceptor.java
+++ src/main/java/com/service/interceptor/RequestInterceptor.java
@@ -6,8 +6,10 @@ import io.jsonwebtoken.Claims;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
+@Provider
 public class RequestInterceptor implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
