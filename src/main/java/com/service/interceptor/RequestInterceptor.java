383c4985026f09f8d5e1acc1c39b26d23f98c015
Jay <jay@Jays-MacBook-Pro.local>
2019-03-18 13:29:11 +0530

    handling Token authorization at request interceptor

---
+++ src/main/java/com/service/interceptor/RequestInterceptor.java
@@ -0,0 +1,26 @@
+package com.service.interceptor;
+
+import com.auth.JWTAuth;
+import io.jsonwebtoken.Claims;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import java.io.IOException;
+
+public class RequestInterceptor implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String authorizationToken = requestContext.getHeaderString("Authorization");
+        try {
+            Claims claims = JWTAuth.decodeJwt(authorizationToken);
+            if(claims != null) {
+                System.out.println("Authorization Success");
+            }
+        } catch (Exception e) {
+            throw new BadRequestException();
+        }
+
+
+    }
+}
